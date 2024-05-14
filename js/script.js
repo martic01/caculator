a89d9f922913fcd6d72b6ee6f4227b5ec4fe064a
matthew aboyade <aboyadematthew@gmail.com>
2024-05-14 15:42:11 -0700

    add write functions to input values

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $(".num").click(function () {
+        let defaultValue = parseInt($("#fvalue").text());
+        let TakenValue = parseInt($(this).find("p").text());
+        let newValue = defaultValue + TakenValue;
+        $("#fvalue").text(newValue);
+    });
+});
+
+
+
+// const numClicked = parseInt($("#fvalue").find("p").text());
+
+// function caculate(numClicked) {
+//     let defaultValue = numClicked;
+//     let TakenValue = parseInt($(this).find("p").text())
+//     let newValue = defaultValue + TakenValue;
+//     return newValue
+// }
+
+
+
+// $(document).ready(function () {
+//     $(".num").click(function () {
+//  let ans = caculate(numClicked)
+//     $("#fvalue").text(ans);
+//     });
+// });
+

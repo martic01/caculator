ab0b9f8c1cfc4663dbd1dd6a047c114a60116918
matthew aboyade <aboyadematthew@gmail.com>
2024-05-14 16:19:54 -0700

    add write functions not working so commeted out writin a new click function

--- js/script.js
+++ js/script.js
@@ -9,21 +9,21 @@ $(document).ready(function () {
 
 
 
-// const numClicked = parseInt($("#fvalue").find("p").text());
+const numClicked = parseInt($("#fvalue").find("p").text());
 
-// function caculate(numClicked) {
-//     let defaultValue = numClicked;
-//     let TakenValue = parseInt($(this).find("p").text())
-//     let newValue = defaultValue + TakenValue;
-//     return newValue
-// }
+function caculate(numClicked) {
+    let defaultValue = numClicked;
+    let TakenValue = parseInt($(this).find("p").text())
+    let newValue = defaultValue + TakenValue;
+    return newValue
+}
 
 
 
-// $(document).ready(function () {
-//     $(".num").click(function () {
-//  let ans = caculate(numClicked)
-//     $("#fvalue").text(ans);
-//     });
-// });
+$(document).ready(function () {
+    $(".num").click(function () {
+ let ans = caculate(numClicked)
+    $("#fvalue").text(ans);
+    });
+});
 
